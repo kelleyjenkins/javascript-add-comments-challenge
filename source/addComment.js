3427f7e8875deea27819aacc02ee4a9d9fe06507
Kelley Jenkins <ckjenkins3@gmail.com>
2018-03-11 16:57:54 -0600

    done

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  // add your code here
+  $("#new_comment_button").click(function(e) {
+    $('#new_comment').show()
+});
+$("#new_comment").submit(function(e) {
+  e.preventDefault()
+  let commentBody = $('textarea[name="comment"]').val()
+  let commentAuthor = $('input[id="authorName"]').val()
+  $("#comment_list").append("<li>" + commentBody + "<span class='author'>" + commentAuthor +
+  "</span></li>")
+  $('#new_comment').hide()
 })
+
+});
